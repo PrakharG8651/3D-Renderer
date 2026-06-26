ba820427a57bcefa3b7c0212ed4a1f8d3f52368e
PrakharG8651 <prakhargupta1568@gmail.com>
2026-06-27 00:40:47 +0530

    Added vertex.java

---
+++ Vertex.java
@@ -0,0 +1,10 @@
+public class Vertex {
+    public double x, y, z;
+
+    // Constructor to initialize the vertex position
+    public Vertex(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
