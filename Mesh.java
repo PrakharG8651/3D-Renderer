2ff5b8c372ca464af9ac52bcd91eb2a5dbd7bce0
PrakharG8651 <prakhargupta1568@gmail.com>
2026-06-27 04:29:16 +0530

    Added Mesh

---
+++ Mesh.java
@@ -0,0 +1,29 @@
+import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Mesh {
+    public List<Triangle> triangles;
+
+    public Mesh(List<Triangle> triangles) {
+        this.triangles = triangles;
+    }
+
+    // This allows you to move any shape to a new location in 3D space easily
+    public void translate(double dx, double dy, double dz) {
+        Set<Vertex> moved = new HashSet<>();
+        for (Triangle t : triangles) {
+            translateVertex(t.v1, dx, dy, dz, moved);
+            translateVertex(t.v2, dx, dy, dz, moved);
+            translateVertex(t.v3, dx, dy, dz, moved);
+        }
+    }
+
+    private void translateVertex(Vertex v, double dx, double dy, double dz, Set<Vertex> moved) {
+        if (moved.add(v)) {
+            v.x += dx;
+            v.y += dy;
+            v.z += dz;
+        }
+    }
+}
